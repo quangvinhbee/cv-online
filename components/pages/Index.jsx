badff44e529e7ae54b8751c4f61fd316a1ebb009
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-26 21:56:22 +0700

    update

--- components/pages/Index.jsx
+++ components/pages/Index.jsx
@@ -9,13 +9,16 @@ const Index = () => {
                     <Container maxWidth="lg">
                         <div className="index-caption">
                             <h1>Hi! I'm Quang Vinh Bee</h1>
-                            <h3>Design Web, Designer, Front end developer</h3>
+                            <h3>Design Web, Front end developer - ReactJS</h3>
                         </div>
                     </Container>
                 </div>
                 <div className="session-2">
                     <Container maxWidth="lg">
-
+                        <div className="header-session">
+                            <h3>ABOUT ME</h3>
+                            <h4>GET TO KNOW ME BEFORE YOU DIVE INTO MY CONTENT.</h4>
+                        </div>
                     </Container>
                 </div>
             </div>
