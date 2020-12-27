922be884f4100410feaa3062616ad7b24c11ca52
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-27 12:50:08 +0700

    update my story

--- components/pages/Index.jsx
+++ components/pages/Index.jsx
@@ -23,7 +23,67 @@ const Index = () => {
                         <img src="images/avt/1.png" alt="" />
                     </div>
                     <div className="caption">
-                        <p>I am a self-employed software and web engineer dedicated to learning and teaching JavaScript for client-server architectures. After obtaining my Master's Degree in computer science, I gained experience from the startup world, where I used JavaScript intensively during both my professional life and spare time. Eventually it led me to teach others about these topics and to offer online courses and on-site consulting for companies. I am happy to welcome you on my website :-)</p>
+                        <p>
+                            I am Lam Vinh,
+                            I live in HCMC and am studying at the Post and Telecommunication Institute of Technology, HCMC.
+                            I'm a 4th year student majoring in information security.
+                            I love web programming, I have been learning web programming since my freshman year.
+                            During my study, I learned some frameworks such as Wordpress, Spring, ReactJS, NextJS... And I did some related work. Please see my story for more info.
+                            Thank you for visiting the website <i class="fa fa-heart red"></i>
+                        </p>
+                    </div>
+                </div>
+                <div className="session-3">
+                    <div className="header-session">
+                        <h3>VITA</h3>
+                        <h4>MY WORK STORY</h4>
+                    </div>
+                    <div className="my-story">
+                        <ul className="line">
+                            <li>
+                                <div className="images-story">
+                                    <img src="images/avt/1.png" />
+                                </div>
+                                <div className="caption-story text-right mr500">
+                                    <h3>2014-2017</h3>
+                                    <h3>Binh Minh High School</h3>
+                                    <p>story story story story story story story story story story </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="images-story">
+                                    <img src="images/avt/1.png" />
+                                </div>
+                                <div className="caption-story text-left ml500">
+                                    <h3>2014-2017</h3>
+                                    <h3>Binh Minh High School</h3>
+                                    <p>story story story story story story story story story story </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="images-story">
+                                    <img src="images/avt/1.png" />
+                                </div>
+                                <div className="caption-story text-right mr500">
+                                    <h3>2014-2017</h3>
+                                    <h3>Binh Minh High School</h3>
+                                    <p>story story story story story story story story story story </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="images-story">
+                                    <img src="images/avt/1.png" />
+                                </div>
+                                <div className="caption-story text-left ml500">
+                                    <h3>2014-2017</h3>
+                                    <h3>Binh Minh High School</h3>
+                                    <p>story story story story story story story story story story </p>
+                                </div>
+                            </li>
+                            <li className='end-story'>
+                                <h4>Be part of my story.</h4>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
