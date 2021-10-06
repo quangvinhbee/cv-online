2f3030c55ab490caa339b3e2b3fcba218517e1c3
quangvinhbee <lamquangvinh44@gmail.com>
2021-10-06 11:25:50 +0700

    Fix: introduction

--- components/pages/component/introduction.jsx
+++ components/pages/component/introduction.jsx
@@ -11,13 +11,13 @@ export function Introduction() {
       <div className="caption">
         <p>
           I am Lam Vinh, I am studying at the Post and Telecommunication
-          Institute of Technology, HCMC. I'm a 4th year student majoring in
+          Institute of Technology, HCMC. I'm a final-year student majoring in
           Information Security. I love web programming, I have been learning web
           programming since my freshman year. During my study, I learned some
-          frameworks such as Wordpress, Bootstrap, ReactJS, NextJS... And I did
-          some related work. I am looking for a job full time in Front end web
-          programming (React JS). Thank you for visiting the website{" "}
-          <i className="fa fa-heart red"></i>
+          frameworks such as Wordpress, Bootstrap, ReactJS, NextJS,
+          TailwindCSS... And I did some related work. I am looking for a job
+          full time in Front end web programming (React JS). Thank you for
+          visiting the website <i className="fa fa-heart red"></i>
         </p>
       </div>
     </div>
